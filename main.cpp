e88464a8f11e3258b6a994679a61177da1884245
ValeryDrozd <drozd.ione@gmail.com>
2020-03-09 20:53:29 +0200

    Added making graph from the labyrint

--- main.cpp
+++ main.cpp
@@ -2,23 +2,15 @@
 using namespace std;
 
 map <int, vector <int> > graph;
-int n,m,start,fin;
+int n,m,start,fin,start,fin;
 vector <int> v;
 int* graphlist;
-int** maze;
 void readGraph(char filename[]){
     ifstream f(filename);
     if(!f){cout<<"Error during reading file\n";exit(1);}
     f>>n>>m;
     graphlist = new int[n*m+1];
     *graphlist = {-1};
-    maze = new int* [n+1];
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++)
-            maze[i] = new int[m+1];
-        for(int j=1;j<=n;j++)
-            maze[i][j] = 0;
-    }
     for(int i=0;i<=n*m+1;i++)
         graphlist[i] = -1;
     f.ignore(256,'\n');
@@ -27,14 +19,29 @@ void readGraph(char filename[]){
         getline(f,s);
         s = " " + s;
         for(int j=1;j<=m*2+1;j+=2){
-            if(s[j]==' '){
-                maze[i][j/2+1] = 1;
+            if(s[j]!='X'){
                 graphlist[(i-1)*n+(j/2)+1] = 0;
+                if(s[j] == 'F')fin = (i-1)*n+(j/2)+1;
+                else
+                    if(s[j] == 'S')start = (i-1)*n+(j/2)+1;
             }
         }
     }
     f.close();
     cout<<"Read completed\n";
+}
+
+void makeGraph(){
+    for(int i=1;i<=n*m;i++){
+        if(a[i]!=-1){
+            if(i - m > 0 && a[i-m]!=-1){graph[i].push_back(i-m);}
+            if(i + m <= n*m && a[i+m]!=-1){graph[i].push_back(i+m);}
+            if((i+1)%m!=0 && i!=n*m && a[i+1]!=-1){graph[i].push_back(i+1);}
+            if((i+1)%m!=1 && i!=1 && a[i-1]!=-1){graph[i].push_back(i-1);}
+
+        }
+    }
+    cout<<"Graph is made\n";
 }
 
 void djikstra(){
@@ -65,6 +72,7 @@ int main(){
     cout<<"Enter path to enter file\n";
     gets(filename);
     readGraph(filename);
+    makeGraph();
     /*djikstra();
     get_path();*/
     cout<<"Enter path to out the file\n";
